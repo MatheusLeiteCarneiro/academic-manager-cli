030c98e8aab2561e9848b257262800052b43fb2e
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-18 20:54:13 -0300

    feat: create Student and Course attributes and methods

--- src/model/entities/Course.java
+++ src/model/entities/Course.java
@@ -1,4 +1,23 @@
 package model.entities;
 
 public class Course {
+    private int id;
+    private String name;
+
+    public Course(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/model/entities/Student.java
+++ src/model/entities/Student.java
@@ -1,4 +1,23 @@
 package model.entities;
 
 public class Student {
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
