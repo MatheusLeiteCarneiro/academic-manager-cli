1cec9370ba6fe675ebbbcacd688adfd7fffc0284
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-19 11:14:14 -0300

    feat: override toString method

--- src/model/entities/Course.java
+++ src/model/entities/Course.java
@@ -20,4 +20,8 @@ public class Course {
     public void setName(String name) {
         this.name = name;
     }
+    @Override
+    public String toString() {
+        return "ID: " + getId() + "| " + getName();
+    }
 }
