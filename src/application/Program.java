3864eca09c0bdc83004563d4ca7fe38d208042f1
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-19 10:24:37 -0300

    feat: create the print menu method

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,4 +14,30 @@ public class Program {
 
         sc.close();
     }
+    private static void printMenu(){
+        System.out.println("--- Operations Menu ---\n");
+
+        System.out.println("-- Records Managements --");
+        System.out.println("1. Add Student");
+        System.out.println("2. Add Course");
+        System.out.println("3. Delete Student");
+        System.out.println("4. Delete Course");
+
+        System.out.println("\n-- Enrollments --");
+        System.out.println("5. Enroll Student in Course");
+        System.out.println("6. Remove Student from Course");
+
+        System.out.println("\n-- Reports --");
+        System.out.println("7. View Student's Courses");
+        System.out.println("8. View Course's Students");
+        System.out.println("9. List All Students");
+        System.out.println("10. List All Courses");
+
+        System.out.println("\n-- System --");
+        System.out.println("0. Exit");
+
+        System.out.print("\nChoose an option: ");
+    }
+
+
 }
