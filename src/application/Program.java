c1d69a80cffc4e79983e26a1b047983f866b8d66
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-18 23:08:33 -0300

    feat: initialize Program main class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,17 @@
 package application;
 
+import model.services.AcademicService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        AcademicService academicService = new AcademicService();
+        System.out.println("Welcome to the academic manager system!\n");
 
+        sc.close();
     }
 }
