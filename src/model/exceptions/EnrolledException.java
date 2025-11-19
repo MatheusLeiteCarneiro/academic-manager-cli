b3070657a2d8ad2061fe6caba94f9bd4c7df94ef
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-18 22:26:09 -0300

    refactor: rename exception to EnrolledException

---
+++ src/model/exceptions/EnrolledException.java
@@ -0,0 +1,7 @@
+package model.exceptions;
+
+public class EnrolledException extends RuntimeException {
+    public EnrolledException(String message) {
+        super(message);
+    }
+}
--- src/model/exceptions/StudentNotEnrolledException.java
+++
@@ -1,7 +0,0 @@
-package model.exceptions;
-
-public class StudentNotEnrolledException extends RuntimeException {
-    public StudentNotEnrolledException(String message) {
-        super(message);
-    }
-}
