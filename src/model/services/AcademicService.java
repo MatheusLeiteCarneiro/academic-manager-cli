57219d15c27e32b9f45c8f0f1dfc1fa537385016
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-18 21:18:51 -0300

    feat: create AcademicService attributes and getters

--- src/model/services/AcademicService.java
+++ src/model/services/AcademicService.java
@@ -1,4 +1,33 @@
 package model.services;
 
+import model.entities.Course;
+import model.entities.Student;
+import model.exceptions.NonExistentId;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class AcademicService {
+    private Map<Integer, Student> studentsMap;
+    private Map<Integer, Course> coursesMap;
+    private Map<Student, Set<Course>> enrollments;
+
+    public AcademicService() {
+    }
+
+    public Map<Integer, Student> getStudentsMap() {
+        return studentsMap;
+    }
+
+    public Map<Integer, Course> getCoursesMap() {
+        return coursesMap;
+    }
+
+
+    public Map<Student, Set<Course>> getEnrollments() {
+        return enrollments;
+    }
+
+
 }
